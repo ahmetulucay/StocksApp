ff73f00043216bdbc01b431650d39d49c925973c
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-04-28 21:52:14 +0200

    Add project files.

---
+++ StocksApp/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StocksApp.Controllers;
+public class HomeController : Controller
+{
+    [Route("/")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
